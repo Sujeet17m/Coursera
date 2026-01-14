a4445a850c6abac763d0fb7eff986ca6884a0dd3
Sujeet17m <sujeet17das@gmail.com>
2026-01-14 15:23:04 +0530

    added skeleton users

--- index.js
+++ index.js
@@ -2,15 +2,13 @@ const express = require('express');
 const app = express();
 const port = 3000;
 
-app.get('/', (req, res) => {
-  res.send('Hello World!');
-});
+app.use(express.json());
 
-app.post('/signup', (req,res) => {
+app.post('/user/signup', (req,res) => {
     res.send('Signup Page');
 });
 
-app.get('/login', (req,res) => {
+app.post('/user/login', (req,res) => {
     res.send('Login Page');
 });
 
@@ -18,11 +16,15 @@ app.get('/logout', (req,res) => {
     res.send('Logout Page');
 });
 
-app.post('/purchase', (req, res) => {
+app.post('/course/purchase', (req, res) => {
     res.send('Purchase Page');
 });
 
-app.get('/profile', (req, res) => {
+app.get('/user/purchases', (req, res) => {
+    res.send('Purchases Page');
+});
+
+app.get('/courses', (req, res) => {
     res.send('Profile Page');
 });
 
