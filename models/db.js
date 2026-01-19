eca7c593fd9f571922b4463a33179a3d482ae5c8
Sujeet17m <sujeet17das@gmail.com>
2026-01-19 23:10:16 +0530

    admin signup

--- models/db.js
+++ models/db.js
@@ -22,6 +22,7 @@ const courseSchema = new Schema({
 
 const adminSchema = new Schema({
     username: {type: String, required: true, unique: true},
+    email: {type: String, required: true, unique: true, validate: [validator.isEmail, 'Invalid email']},
     password: {type: String, required: true},
     firstName: {type: String},
     lastName: {type: String}
--- routes/admin.js
+++ routes/admin.js
@@ -1,9 +1,28 @@
 const {Router} = require('express');
 const adminRouter = Router();
 const {AdminModel, CourseModel} = require('../models/db');
+const bcrypt = require('bcrypt');
+const jwt = require('jsonwebtoken');
+const validator = require('validator');
 
-adminRouter.post('/signup', (req, res) => {
-    res.send('Admin Signup Page');
+adminRouter.use(require('express').json());
+
+
+adminRouter.post('/signup', async (req, res) => {
+    const adminData = new AdminModel({
+            username: req.body.username,
+            email: req.body.email,
+            password: await bcrypt.hash(req.body.password, 10),
+            firstName: req.body.firstName,
+            lastName: req.body.lastName,
+        });
+    
+        try {
+            await adminData.save();
+            res.status(201).json({ message: 'Admin created successfully' });
+        } catch (error) {
+            res.status(400).json({ error: error.message });
+        }
 });
 
 adminRouter.post('/login', (req, res) => {
